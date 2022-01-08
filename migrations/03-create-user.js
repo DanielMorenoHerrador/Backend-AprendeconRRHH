2ea7653db34bd10b78c483152579ec98dd3ef5db
DanielMorenoHerrador <90707206+DanielMorenoHerrador@users.noreply.github.com>
2022-01-08 19:11:59 +0100

    implementar JWT para que si quieres ver los servicios y pedidos, tengas que estar logeado

--- migrations/03-create-user.js
+++ migrations/03-create-user.js
@@ -2,7 +2,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('users', {
-      userId: {
+      id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
